66592c49af0994c4f0fa418c6eb648e941209588
khalifa1982 <1.63648156e+08+khalifa1982@users.noreply.github.com>
2026-07-29 23:14:33 +0000

    v1.0.26: Update Firebase config files with correct bundle ID (com.app.relaymobile)

--- app.config.ts
+++ app.config.ts
@@ -47,7 +47,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.0.25",
+  version: "1.0.26",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
